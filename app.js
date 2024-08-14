257ec2ca7361081241d6de886b4d46004604e2c0
yusef carr <ycarr99@gmail.com>
2024-08-14 15:03:28 -0400

    new push

---
+++ app.js
@@ -0,0 +1,28 @@
+var waffleCrisp = 1;
+var trix = 2;
+var honeyBunchesOfOats = 3;
+var fruityPebbles = 4;
+var honeyNutCheerios = 5;
+var life = 6;
+var raisinBran = 7;
+var oreoCereal = 8;
+var smacks = 9;
+var cocoPuffs = 10;
+
+console.log(waffleCrisp + honeyBunchesOfOats);
+console.log(trix + cocoPuffs);
+
+console.log(smacks - honeyBunchesOfOats);
+console.log(oreoCereal - fruityPebbles);
+
+console.log(waffleCrisp * raisinBran);
+console.log(life * honeyNutCheerios);
+
+console.log(cocoPuffs / trix);
+console.log(oreoCereal / waffleCrisp);
+
+console.log(waffleCrisp ++);
+console.log(smacks ++ );
+
+console.log(fruityPebbles --);
+console.log(life --);
